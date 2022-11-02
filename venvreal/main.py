1f95f532ab3db44512676c8e46464046ad5a1713
iamcyruss <russnicosia@gmail.com>
2022-11-01 19:54:10 -0600

    added more

--- venvreal/main.py
+++ venvreal/main.py
@@ -56,14 +56,20 @@ while running:
             game_title = input("Are you looking for a specific game title?\ny/n\n")
             if game_title.lower() == 'n':
                 print(cheapshark_response_json)
-                for games in cheapshark_response_json:
-                    for store in store_response_init_json:
-                        if games['storeID'] in store['storeID']:
-                            print(f"Title: {games['title']}\n"
-                                  f"Store Name: {store['storeName']}\n"
-                                  f"Normal Price: {games['normalPrice']}\n"
-                                  f"Sale Price: {games['salePrice']}\n"
-                                  f"Link: {CHEAPSHARP_REDIRECT + games['dealID']}")
+                game_title_check = ''
+                # need to fix the below poop. Trying to check to see if the next title matches the previous title and if so ignore it.
+                # it should look at the previous title and previous sale price and if the same then ignore it.
+                for games_counts, games in enumerate(cheapshark_response_json):
+                    if games['title'] == game_title_check:
+                        game_title_check = cheapshark_response_json[games_counts]['title']
+                    else:
+                        for store_counts, store in enumerate(store_response_init_json):
+                            if games['storeID'] in store['storeID']:
+                                print(f"Title: {games['title']}\n"
+                                      f"Store Name: {store['storeName']}\n"
+                                      f"Normal Price: {games['normalPrice']}\n"
+                                      f"Sale Price: {games['salePrice']}\n"
+                                      f"Link: {CHEAPSHARP_REDIRECT + games['dealID']}")
                 running_game_title = False
             elif game_title.lower() == 'y':
                 user_game_title = input("What's the game title you'd liek to search for?\n")
